09e68e44395bf853808a806b38ca3682b94c1722
Josue <sjosue5082002@gmail.com>
2022-01-31 18:25:32 -0600

    Crear baraja de cartas

---
+++ assets/js/juego.js
@@ -0,0 +1,30 @@
+/**
+ * 2C = Two of Clubs (treboles)
+ * 2D = Two of Diamonds (diamantes)
+ * 2H = Two of Hearts (corazones)
+ * 2S = Two of Spades (picas)
+ */
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['J', 'Q', 'K', 'A'];
+
+const crearDeck = () => {
+
+    for(let i = 2; i <= 10; i++) {
+        for(let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+    for(let tipo of tipos) {
+        for(let esp of especiales){
+            deck.push(esp + tipo)
+        }
+    }
+    console.log(deck);
+
+    deck = _.shuffle(deck);
+    console.log(deck);
+    return deck;
+}
+crearDeck();
