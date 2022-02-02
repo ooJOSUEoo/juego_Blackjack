1fa6ce13616b745fedbc6fd0359507fb121e5757
Josue <sjosue5082002@gmail.com>
2022-02-01 19:52:54 -0600

    Turno de la computadora

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -18,6 +18,7 @@ const btnDetener = document.querySelector('#btnDetener');
 const btnNuevo = document.querySelector('#btnNuevo');
 
 const divCartasJuagdor = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
 const puntosHtml = document.querySelectorAll('small');
 
 //crear el mazo
@@ -65,6 +66,24 @@ const valorCarta = (carta) => {
     // }
 }
 
+//turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+    do{
+        const carta = pedirCarta();
+        puntosComputadora += valorCarta(carta);
+        puntosHtml[1].innerHTML = puntosComputadora;
+
+        //<img class="carta" src="assets/cartas/2H.png" alt="">
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append(imgCarta);
+        if(puntosJugador > 21){
+            break;
+        }
+    }while((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+}
+
 //eventos
 btnPedir.addEventListener('click', () => {
     const carta = pedirCarta();
@@ -82,10 +101,18 @@ btnPedir.addEventListener('click', () => {
         console.log('Perdiste');
         btnPedir.disabled = true;
         btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     }else if (puntosJugador == 21){
         console.log('Ganaste');
         btnPedir.disabled = true;
         btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
 
+})
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
 })
